736c55d5eda8fb8a13b0e1e25c542176c803b728
MHG1816 <hammaguibeneess@gmail.com>
2024-02-12 10:40:27 +0100

    avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -15,6 +15,7 @@ const Avatar: React.FC<AvatarProps> = ({
         <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
             <Image alt="Avatar" src={user?.image || "/images/placeholder.jpg"} fill/>
         </div>
+        <div className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3"/>
     </div>
   )
 }
