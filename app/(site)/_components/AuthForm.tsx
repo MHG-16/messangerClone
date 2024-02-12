e18740bbd946f526b35d82ea483e0428a6069a81
MHG1816 <hammaguibeneess@gmail.com>
2024-02-12 09:20:15 +0100

    Update AuthForm to use  and  && siedbar desktop

--- app/(site)/_components/AuthForm.tsx
+++ app/(site)/_components/AuthForm.tsx
@@ -2,20 +2,29 @@
 
 import Button from "@/components/button";
 import Input from "@/components/input";
-import { startTransition, useCallback, useState, useTransition } from "react";
+import { startTransition, useCallback, useEffect, useState, useTransition } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub } from "react-icons/bs";
 import { FaGoogle } from "react-icons/fa";
 import axios from "axios";
 import toast from "react-hot-toast";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
     const [variant, setVariant] = useState<variant>("LOGIN");
     const [isPending, startTranstion] = useTransition();
+    const session = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session?.status === "authenticated") {
+            router.push("/users");
+        }
+    }, [session?.status, router]);
 
     const toggleVariant = useCallback(() => {
         variant === "LOGIN" ? setVariant("REGISTER") : setVariant("LOGIN");
@@ -39,6 +48,7 @@ const AuthForm = () => {
         startTranstion(() => {
             if(variant === "REGISTER") {
                 axios.post("/api/register", data)
+                .then(() => signIn("credentials", data))
                 .catch(() => toast.error("Something went wrong"))
                 }
             else {
