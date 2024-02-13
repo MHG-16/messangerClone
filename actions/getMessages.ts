ca9938b2e09810c0053c1e7897ab9f210ea6e408
MHG1816 <hammaguibeneess@gmail.com>
2024-02-13 15:39:46 +0100

    getMessages

---
+++ actions/getMessages.ts
@@ -0,0 +1,19 @@
+import { db } from "@/lib/db";
+
+const getMessages = async (conversationId: string) => {
+    try {
+        const messages = await db.message.findMany({
+            where: { conversationId: conversationId },
+            include: {
+                sender: true,
+                seen: true,
+            },
+            orderBy: {
+                createdAt: "asc"
+            }
+        });
+    return messages;
+    } catch {
+        return [];
+    }
+}
