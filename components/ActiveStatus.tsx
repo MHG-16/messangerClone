2147bceb2ffa1f5efd9c920b33f11ea5bef8c5cc
MHG1816 <hammaguibeneess@gmail.com>
2024-02-16 16:20:29 +0100

    use active channel

---
+++ components/ActiveStatus.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import useActiveChannel from "@/hooks/useActiveChannels";
+
+const ActiveStatus = () => {
+    useActiveChannel();
+    return null;
+}
+
+export default ActiveStatus;
